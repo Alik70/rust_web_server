de86fb1233e76097309502b1c6dda1fe014130cc
Ali Karimi <ak.msm91@gmail.com>
2021-12-12 18:10:10 +0330

    Minor refactoring.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,6 +3,7 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::time::Duration;
+
 use web_server::ThreadPool;
 
 fn main() {
@@ -40,14 +41,14 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let (status_line, filename) =
-    if buffer.starts_with(get) {
-        ("HTTP/1.1. 200 OK", "index.html")
-    } else if buffer.starts_with(sleep){
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1. 200 OK", "index.html")
-    } else {
-        ("HTTP/1.1. 404 NOT FOUND", "404.html")
-    };
+        if buffer.starts_with(get) {
+            ("HTTP/1.1. 200 OK", "index.html")
+        } else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1. 200 OK", "index.html")
+        } else {
+            ("HTTP/1.1. 404 NOT FOUND", "404.html")
+        };
     let contents = fs::read_to_string(filename).unwrap();
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
